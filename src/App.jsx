ad453bae200215de0c0f37b3f308a2a681e16360
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2024-04-20 08:11:19 +0000

    responsiveness + icon added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ function App() {
   const [quotes , setQuotes] = useState([])
   const [quote , setQuote] = useState('Loading....')
 
-  function getQuote(){
 
-    
+  useEffect(()=>{
     fetch('https://type.fit/api/quotes')
     .then( res => res.json())
     .then( data => setQuotes(data))
-    
+  },[])
+
+  function getQuote(){    
     let num = Math.floor(Math.random()* quotes.length)
     console.log(quotes[num])
     setQuote(quotes[num].text)
@@ -20,10 +21,10 @@ function App() {
 
   return (
     <div className='mt-16'>
-      <h1 className='m-4  inline-block p-5 text-4xl w-[50vw] font-semibold rounded-xl bg-[#3dd1e7]'>{quote}</h1>
+      <h1 className='m-4  inline-block p-5 text-xl md:text-4xl md:w-[50vw] font-semibold rounded-xl bg-[#3dd1e7]'>{quote}</h1>
       <br />
       <button onClick={getQuote}
-      className='border border-none p-3 font-semibold text-2xl rounded-xl hover:bg-white/70 bg-[#e7da3d] '
+      className='border border-none p-3 font-semibold md:text-2xl rounded-xl hover:bg-white/70 bg-[#e7da3d] '
       >Generate Quote</button>
     </div>
   )
